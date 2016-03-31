e9c1b459350f6f686fe1b48a6f059ae532491764
Nguyen Ba Cuong <cuongnb14@gmail.com>
2016-03-31 10:55:56 +0700

    add setup hamachi script

---
+++ hamachi/setup.sh
@@ -0,0 +1,4 @@
+sudo apt-get install lsb
+wget https://secure.logmein.com/labs/logmein-hamachi_2.1.0.139-1_amd64.deb
+sudo dpkg -i logmein-hamachi_2.1.0.139-1_amd64.deb
+sudo service logmein-hamachi start 
