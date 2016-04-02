7d002234a2c836733abeba9ee25f6ca0d383b782
Nguyen Ba Cuong <cuongnb14@gmail.com>
2016-04-02 12:14:11 +0700

    login affter install

--- hamachi/setup.sh
+++ hamachi/setup.sh
@@ -2,3 +2,4 @@ sudo apt-get install lsb
 wget https://secure.logmein.com/labs/logmein-hamachi_2.1.0.139-1_amd64.deb
 sudo dpkg -i logmein-hamachi_2.1.0.139-1_amd64.deb
 sudo service logmein-hamachi start 
+sudo hamachi login
